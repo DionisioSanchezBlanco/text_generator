2227d3c7cd70a92dea54974d7c5d3cc8f1cc0dc8
Dioni00 <dsanbla@gmail.com>
2021-03-21 18:44:07 +0100

    Stage 1. To check

--- text_generator.py
+++ text_generator.py
@@ -1,1 +1,24 @@
 # Stage 1. Text Generator
+from nltk.tokenize import word_tokenize
+
+f = open("corpus.txt", "r", encoding="utf-8")
+
+# We get tokens from the corpus
+wtk = word_tokenize(f.read())
+
+print("Corpus statistics")
+print(f"All tokens: {len(wtk)}")
+
+# Create a set for unique words. Sets don't allow duplicate values
+print(f"Unique tokens: {len(set(wtk))}\n")
+
+option = None
+while option != "exit":
+    try:
+        print(wtk[int(input())])
+    except ValueError:
+        print("Type Error. Please input an integer.")
+    except IndexError:
+        print("Index Error. Please input an integer that is in the range of the corpus.")
+
+f.close()
