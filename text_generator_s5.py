fd14a2d48786a50c40af99947055be0216bdebc5
DSanchez <dsanbla@gmail.com>
2021-04-02 12:16:34 +0200

    Stage 5. Generate full sentence

---
+++ text_generator_s5.py
@@ -0,0 +1,76 @@
+# Stage 5. Generate full sentences
+# Hint: The tail chosen will become in the new head for the next iteration
+
+from nltk.tokenize import WhitespaceTokenizer
+from nltk import bigrams
+from collections import Counter
+import random
+import re
+
+
+# To get the a random head from the full tokens list to initialize the sentence
+# Match to ini_word
+def get_head(head, ini_word):
+    result = ""
+    while not result:
+        head = random.choice(wtk)
+        result = re.match(ini_word, head)
+
+    return head
+
+
+# To get the final word matching to RegExp
+def get_final(tail, final_word):
+    return re.match(final_word, tail)
+
+
+# f = open("../test/corpus.txt", "r", encoding="utf-8")
+f = open(input(), "r", encoding="utf-8")
+
+# We get tokens from the corpus
+wtk = WhitespaceTokenizer().tokenize(f.read())
+
+# Bigrams generates an iterator. Put type list to get the data
+bigr = list(bigrams(wtk))
+dict_bigr = {}
+
+# We create a dictionary
+# Key = First value in the tuple
+# Value = Second value that we store as a list associated to the key
+for key, value in bigr:
+    dict_bigr.setdefault(key, []).append(value)
+
+# RegExp templates
+ini_word = "[A-Z].+[^\.\!\?]$"
+final_word = "[A-z].+[\.\!\?]"
+
+head = None
+i = 1
+sentence = []
+
+while i <= 10:
+    # To get different seed each time
+    random.seed()
+    head = get_head(head, ini_word)
+
+    sentence.append(head)
+    result_final = ""
+
+    while not result_final:
+        list_tail = [tail for tail in dict_bigr[head]]
+        dict_tail = Counter(list_tail)  # Dictionary with keys and times repeated
+        word_tail = [key for key in dict_tail.keys()]
+        weight_tail = [value for value in dict_tail.values()]
+
+        head = "".join(random.choices(word_tail, weights=weight_tail, k=1))
+        result_final = get_final(head, final_word)
+        sentence.append(head)
+        if result_final and len(sentence) < 5:
+            result_final = None
+
+    print(*sentence)
+    sentence.clear()
+
+    i += 1
+
+f.close()
